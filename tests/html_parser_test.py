bf4a39e6fb54d0cf4db81eaaf9b4188a104cfacc
Marcus Xavier <marcusxavierr123@gmail.com>
2024-09-03 21:07:45 -0300

    test: Update initial game board parsing test case

--- tests/html_parser_test.py
+++ tests/html_parser_test.py
@@ -1,14 +1,13 @@
 import pytest
-from utils.html_parser import convert_to_board
+from utils.html_parser import convert_to_board, ParsedHTML
 
 @pytest.fixture(autouse=True)
 def before_all():
     pass
 
 def test_initial_game_board_parsing():
-    with open("tests/initial_game_board.html", "r") as f:
-        html = f.read()
-
-    board_data = convert_to_board(html)
-    assert board_data.board.shape == (4, 4)
-    assert True
+    parsed_data = [ParsedHTML(value=2, is_new=True, position_x_y=(3,2), position_hash=hash((3,2)), was_merged=False)]
+    board_data = convert_to_board(parsed_data, 0)
+    assert board_data.board.shape == (4,4)
+    assert board_data.board[3][2] == 2
+    assert board_data.board[0][0] == 0
