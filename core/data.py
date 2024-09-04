b0b0ef6bbea79e07a1318dcaf6a94444eb020b7a
Marcus Xavier <marcusxavierr123@gmail.com>
2024-09-04 19:08:42 -0300

    feat(data): add hash and equality methods to BoardData

--- core/data.py
+++ core/data.py
@@ -1,7 +1,13 @@
 from dataclasses import dataclass
-from numpy import ndarray
+from numpy import ndarray, array_equal
 
 @dataclass
 class BoardData:
     board: ndarray
     score: int
+
+    def __hash__(self):
+        return hash(self.board.tobytes())
+
+    def __eq__(self, other):
+        return array_equal(self.board, other.board)
