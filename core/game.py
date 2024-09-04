b8f84af26d5c50542ef4a40c2cd204d3af7d419b
Marcus Xavier <marcusxavierr123@gmail.com>
2024-09-04 19:09:18 -0300

    feat(game): add simulation for determining best move

--- core/game.py
+++ core/game.py
@@ -1,9 +1,20 @@
+from functools import lru_cache
+from typing import cast
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from collections import deque
+
+from core.data import BoardData
+from core.valid_keys import ValidKeys
+# from utils import transiction_board
+import numpy as np
 
 class Game:
-    def __init__(self, driver: WebDriver) -> None:
+    def __init__(self, driver: WebDriver|None, num_of_simulations: int = 6) -> None:
         self.driver = driver
+        self.num_of_simulations = num_of_simulations
+        self.counter = 0
 
     def deal_with_paused_game(self, driver: WebDriver):
         won_the_game = driver.find_elements(By.CSS_SELECTOR, ".game-message.game-won")
@@ -30,4 +41,127 @@ class Game:
 
         driver.find_element(By.CLASS_NAME, "keep-playing-button").click()
         return
+
+    def get_best_next_move(self, board: BoardData) -> str:
+        _, best_move = self._simulate_next_move(board, 0, None)
+        if best_move is None:
+            raise Exception("Erro na lógica: Eu não deveria passar o None da ultima recursão pra trás")
+
+        if best_move is ValidKeys.UP:
+            return Keys.UP
+        if best_move is ValidKeys.DOWN:
+            return Keys.DOWN
+        if best_move is ValidKeys.LEFT:
+            return Keys.LEFT
+        return Keys.RIGHT
+
+    @lru_cache(maxsize=None)
+    def _simulate_next_move(self, board: BoardData, recurse_counter: int, last_move: ValidKeys|None) -> tuple[BoardData, ValidKeys|None]:
+        if recurse_counter == self.num_of_simulations:
+            return board, None
+
+        boards: list[tuple[BoardData, ValidKeys|None]] = [(board, ValidKeys.UP)]
+        # Criar uma função que me retorne os movimentos possíveis pro tabuleiro :(
+        # TODO: Depois eu vou testar fazer uma A* search ou uma Greedy search aqui. Ao inves de expandir todos os nós, eu vou simular todo um nível, e só expandir o que parece melhor. Ou então simular 3 níveis, e depois expandir isso
+        for move in [ValidKeys.UP, ValidKeys.DOWN, ValidKeys.LEFT, ValidKeys.RIGHT]:
+            if move == last_move:
+                continue
+
+            b = transiction_board(board, move)
+            if b.score == -1:
+                continue
+            b, _ = self._simulate_next_move(b, recurse_counter + 1, move)
+            boards.append((b, move))
+
+        return max(boards, key=lambda x: x[0].score)
+
+# TODO: Resolver esse import circular depois
+def transiction_board(data: BoardData, move: ValidKeys) -> BoardData:
+    new_board = move_board(data, move)
+    if np.array_equal(new_board.board, data.board):
+        return BoardData(new_board.board, -1)
+    new_board.board = insert_value_at_random_empty_position(new_board.board)
+
+    return new_board
+
+# TODO: Refatorar pra remover a parte repetitiva de dentro dos ifs
+def move_board(data: BoardData, move: ValidKeys) -> BoardData:
+    new_board = np.zeros(data.board.shape)
+    score = data.score
+    if move == ValidKeys.LEFT:
+        for i in range(data.board.shape[0]):
+            new_row, row_score = slide_row_to_left(array_to_tuple(data.board[i]))
+            new_board[i] = new_row
+            score += row_score
+    if move == ValidKeys.RIGHT:
+        for i in range(data.board.shape[0]):
+            # Aqui eu simplesmente inverto o array, e mando fazer o slide pra esquerda
+            new_row, row_score = slide_row_to_left(array_to_tuple(data.board[i][::-1]))
+            # E inverto de volta na hora de colocar no array
+            new_board[i] = new_row[::-1]
+            score += row_score
+    if move == ValidKeys.UP:
+        # Aqui eu transponho pra lidar com o slide pra esquerda (horizontalmente) quando o movimento deveria ser pra cima (verticalmente)
+        transposed = data.board.T
+        for i in range(data.board.shape[0]):
+            new_row, row_score = slide_row_to_left(array_to_tuple(transposed[i]))
+            new_board[i] = new_row
+            score += row_score
+        new_board = new_board.T
+    if move == ValidKeys.DOWN:
+        # Aqui transponho e inverto, como se fosse um movimento pra direita
+        transposed = data.board.T
+        for i in range(data.board.shape[0]):
+            new_row, row_score = slide_row_to_left(array_to_tuple(transposed[i][::-1]))
+            new_board[i] = new_row[::-1]
+            score += row_score
+        new_board = new_board.T
+
+    # E por fim, retorno meu novo board
+    return BoardData(new_board, score)
+
+def array_to_tuple(array: np.ndarray) -> tuple:
+    return tuple(array.tolist())
+
+@lru_cache(maxsize=None)
+def slide_row_to_left(data: tuple) -> tuple[np.ndarray,int]:
+    # Remove todos os valores zerados pra simplificar o trabalho, imagine [0, 2, 0, 2]
+    # removendo os zeros, fica [2, 2], então fica muito mais obvio que um movimento pra esquerda gera o [4, 0, 0, 0]
+    row = np.array(data)
+    non_zero = row[row != 0]
+    new_row = []
+    score = 0
+    merged = False
+    # Aqui eu percorro cada item do meu array, e se ele for igual ao proximo, eu faço o merge, senão, eu só adiciono no array
+    for i in range(len(non_zero)):
+        if merged:
+            merged = False
+            continue
+        current_value = non_zero[i].astype(int).item()
+        if i + 1 < len(non_zero) and non_zero[i] == non_zero[i+1]:
+            new_row.append(current_value * 2)
+            score += current_value * 2
+            merged = True
+        else:
+            new_row.append(current_value)
+
+    # E por fim, preciso preencher o resto do array com zeros e transformar em um vetor numpy
+    n_of_zeros = len(row) - len(new_row)
+    return np.array(new_row + [0] * n_of_zeros), cast(int, score)
+
+def insert_value_at_random_empty_position(board: np.ndarray) -> np.ndarray:
+    # Aqui eu preciso verificar se existe algum espaço vazio, eu posso inserir o numero 2
+    empty_positions = []
+    for i in range(board.shape[0]):
+        for j in range(board.shape[1]):
+            if board[i][j] == 0:
+                empty_positions.append((i, j))
+
+    if len(empty_positions) == 0:
+        return board
+
+    np.random.shuffle(empty_positions)
+    i, j = empty_positions[np.random.choice(len(empty_positions))]
+    board[i][j] = 2
+    return board
 
