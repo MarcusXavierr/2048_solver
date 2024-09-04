ee19b20b624624fdcf417d70d3ccdb3782db6c0d
Marcus Xavier <marcusxavierr123@gmail.com>
2024-09-04 19:09:40 -0300

    refactor(main): simplify game loop and improve readability

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
-from numpy import random
 from core import Delay, Keyboard, Game
 from utils import init_argparser
+from utils import HTMLHelper
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 
-# TODO: Usar alguma lib de flags pra ler as flags do usuário corretamente
-# Primeiro eu vou botar só um delay
+# TODO: Adicionar lib de .env pra ler os valures default do environment
 def main():
     parser = init_argparser()
     args = parser.parse_args()
@@ -16,14 +15,15 @@ def main():
 
     driver.get("file:///home/marcus/Projects/courses/ufabc-ai/2048/external/play2048.co/index.html")
     gameHelper = Game(driver)
+    htmlHelper = HTMLHelper(driver)
     keyboard = Keyboard(driver)
     delay = Delay()
 
-    for _ in range(1000):
+    while True:
         delay.start_tracking()
         gameHelper.deal_with_paused_game(driver)
-        # Here the desition is made
-        keyboard.press_key(random.choice(keyboard.valid_keys))
+        move = gameHelper.get_best_next_move(htmlHelper.get_board_data())
+        keyboard.press_key(move)
         delay.delay_with_compensation_ms(sleeptime)
 
 if __name__ == '__main__':
